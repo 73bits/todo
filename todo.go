833f7bde0efb8a1e7f818fb16be602407ba73a8b
73bits <prashantkr0628@gmail.com>
2025-12-27 00:15:30 +0530

    exported methods

--- todo.go
+++ todo.go
@@ -17,7 +17,7 @@ type Todo struct {
 
 type Todos []Todo
 
-func (todos *Todos) add(title string) {
+func (todos *Todos) Add(title string) {
 	todo := Todo{
 		Title:       title,
 		Completed:   false,
@@ -28,25 +28,25 @@ func (todos *Todos) add(title string) {
 	*todos = append(*todos, todo)
 }
 
-func (todos *Todos) validateIndex(index int) error {
+func (todos *Todos) ValidateIndex(index int) error {
 	if index < 0 || index >= len(*todos) {
 		return errors.New("invalid index")
 	}
 	return nil
 }
 
-func (todos *Todos) delete(index int) error {
+func (todos *Todos) Delete(index int) error {
 	t := *todos
-	if err := t.validateIndex(index); err != nil {
+	if err := t.ValidateIndex(index); err != nil {
 		return err
 	}
 	*todos = append(t[:index], t[index+1:]...)
 	return nil
 }
 
-func (todos *Todos) toggle(index int) error {
+func (todos *Todos) Toggle(index int) error {
 	t := *todos
-	if err := t.validateIndex(index); err != nil {
+	if err := t.ValidateIndex(index); err != nil {
 		return err
 	}
 	isCompleted := t[index].Completed
@@ -58,16 +58,16 @@ func (todos *Todos) toggle(index int) error {
 	return nil
 }
 
-func (todos *Todos) edit(index int, title string) error {
+func (todos *Todos) Edit(index int, title string) error {
 	t := *todos
-	if err := t.validateIndex(index); err != nil {
+	if err := t.ValidateIndex(index); err != nil {
 		return err
 	}
 	t[index].Title = title
 	return nil
 }
 
-func (todos *Todos) print() {
+func (todos *Todos) Print() {
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
 	defer w.Flush()
 
