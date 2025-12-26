22af3b446e076051e7f0582d8731ca8245dd13a8
73bits <prashantkr0628@gmail.com>
2025-12-26 23:17:14 +0530

    toggle method for completion defined

--- todo.go
+++ todo.go
@@ -40,3 +40,17 @@ func (todos *Todos) delete(index int) error {
 	*todos = append(t[:index], t[index+1:]...)
 	return nil
 }
+
+func (todos *Todos) toggle(index int) error {
+	t := *todos
+	if err := t.validateIndex(index); err != nil {
+		return err
+	}
+	isCompleted := t[index].Completed
+	if !isCompleted {
+		completionTime := time.Now()
+		t[index].CompletedAt = &completionTime
+	}
+	t[index].Completed = !isCompleted
+	return nil
+}
