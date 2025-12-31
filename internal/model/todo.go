c5cc463d88be34820bc033dfd65efa6e65e457b8
73bits <prashantkr0628@gmail.com>
2025-12-31 15:03:01 +0530

    todo model created

---
+++ internal/model/todo.go
@@ -0,0 +1,10 @@
+package model
+
+import "time"
+
+type Todo struct {
+	Title       string     `json:"title"`
+	Completed   bool       `json:"completed"`
+	CreatedAt   time.Time  `json:"created_at"`
+	CompletedAt *time.Time `json:"completed_at"`
+}
