a4b6b96cb98cd6bb8cace1859ba2bcf7ee36b226
73bits <prashantkr0628@gmail.com>
2026-01-03 15:45:14 +0530

    pretty list with text/tabwriter

--- cmd/list.go
+++ cmd/list.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"text/tabwriter"
 
 	"github.com/73bits/todo/internal/service"
 )
@@ -20,3 +22,19 @@ func List(svc *service.TodoService) {
 		fmt.Printf("[%s] %d. %s\n", status, i, t.Title)
 	}
 }
+
+func ListTabWriter(svc *service.TodoService) {
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
+	defer w.Flush()
+
+	todos, err := svc.List()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Fprintln(w, "ID\tTITLE\tCOMPLETED\tCREATED_AT\tCOMPLETED_AT")
+	for i, t := range todos {
+		fmt.Fprintln(w, i, "\t", t.Title, "\t", t.Completed, "\t", t.CreatedAt, "\t", t.CompletedAt)
+	}
+}
+
